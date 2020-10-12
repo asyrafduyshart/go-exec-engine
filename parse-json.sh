d9dd62ea39693daa9bfd82db569c6fd785a02700
Asyraf Duyshart <me@asyraf.id>
2020-10-12 14:48:28 +0700

    - add data schema

--- parse-json.sh
+++ parse-json.sh
@@ -7,6 +7,4 @@
 # JSON read and turned into bash variables
 read dataJson
 eval "$(echo ${dataJson} | jq -r 'to_entries | .[] | .key + "=\"" + (.value|tostring) + "\""')"
-echo ${name} ${age} ${city} 
-
-
+echo ${name} ${age} ${city}
